3fda96eb60921255bb443eb8a762ae1944957dc6
Emirhan Topal <emirhantopal03@hotmail.com>
2025-03-02 17:51:16 +0300

    done v3

--- so_long.h
+++ so_long.h
@@ -18,7 +18,7 @@
 # include "get_next_line/get_next_line.h"
 # include "printf/ft_printf.h"
 
-typedef	struct	s_data
+typedef struct s_data
 {
 	void	*mlx;
 	void	*win;
@@ -27,33 +27,33 @@ typedef	struct	s_data
 	void	g_wall;
 	void	g_collectable;
 	void	g_door;
-	int 	player_x;
-	int 	player_y;
-	int 	screen_x;
-	int 	screen_y;
-	int 	pixel_size;
-	int 	step_counter;
-	int 	rows;
-	int 	*collectable_arr;
-	int 	collectable_count;
-	int 	collectable_index;
-	int 	collected_count;
-	int 	*wall_arr;
-	int 	wall_count;
-	int 	wall_index;
-	int 	*exit_arr;
-	int 	exit_count;
-	int 	player_count;
-	int 	*line_count;
-	int 	line_count_index;
+	int		player_x;
+	int		player_y;
+	int		screen_x;
+	int		screen_y;
+	int		pixel_size;
+	int		step_counter;
+	int		rows;
+	int		*collectable_arr;
+	int		collectable_count;
+	int		collectable_index;
+	int		collected_count;
+	int		*wall_arr;
+	int		wall_count;
+	int		wall_index;
+	int		*exit_arr;
+	int		exit_count;
+	int		player_count;
+	int		*line_count;
+	int		line_count_index;
 	char	**map;
-	int 	flag;
+	int		flag;
 	char	filename;
 	char	game_name;
 	int		u_x;
-	int 	l_y;
-	int 	lw_x;
-	int 	r_y;
+	int		l_y;
+	int		lw_x;
+	int		r_y;
 	int		len;
 }	t_data;
 
