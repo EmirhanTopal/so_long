4e05c7333e6f30a192fa4bb90e6f18c487eae14a
Emirhan Topal <emirhantopal03@hotmail.com>
2025-03-02 18:02:57 +0300

    done v4

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -1,17 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/02 17:54:38 by marvin            #+#    #+#             */
+/*   Updated: 2025/03/02 17:54:54 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
 
-int ft_printf(const char *format, ...);
-int ft_putchar(char c);
-int ft_printf_strlen(const char *str);
-int ft_putnbr(long nbr);
-int ft_putstr(char *str);
-int ft_uputnbr(unsigned int nbr);
-int ft_pointer(void *p);
-int ft_puthexanbr(unsigned long long_nbr, char control);
+int	ft_printf(const char *format, ...);
+int	ft_putchar(char c);
+int	ft_printf_strlen(const char *str);
+int	ft_putnbr(long nbr);
+int	ft_putstr(char *str);
+int	ft_uputnbr(unsigned int nbr);
+int	ft_pointer(void *p);
+int	ft_puthexanbr(unsigned long long_nbr, char control);
 
 #endif
